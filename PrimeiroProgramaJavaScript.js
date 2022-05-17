fcf20e063069c035fa58775f631c8ee220f9fce2
Diego Maciel <diego1504@gmail.com>
2022-05-16 21:30:39 -0300

    primeiro

---
+++ PrimeiroProgramaJavaScript.js
@@ -0,0 +1,12 @@
+console.log("Hello Word!!!");
+console.log("Alguns trabalhos com variaveis");
+
+const idade = 35;
+
+console.log("Print basico de um inteiro" + idade);
+
+console.log("Print basico de um inteiro junto com uma operacao aritimetica" + (idade + 10));
+
+const multiplicaIdade = 35 * 2;
+
+console.log("Print idade multiplicada" + multiplicaIdade);
